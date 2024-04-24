064136e662d69e606b5d9d2cb96515d266a46b37
OsaeJr <samueladjar11@gmail.com>
2024-04-24 02:55:24 -0700

    Add file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
-// Prints a message depending of the number of arguments passed
+// Prints a message depending on the number of arguments passed
 
-if (process.argv.length === 2) {
+// process.argv contains an array of command-line arguments passed to the script
+const argsCount = process.argv.length - 2; // Subtract 2 to exclude 'node' and script path
+
+if (argsCount === 0) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (argsCount === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
+
